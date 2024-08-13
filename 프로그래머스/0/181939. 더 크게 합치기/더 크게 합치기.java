edb75ca45483beec4a1aa0db7a6e8dbac4746fb3
skim257 <85626518+skim257@users.noreply.github.com>
2024-08-13 10:02:55 +0900

    [level 0] Title: 더 크게 합치기, Time: 14.57 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int num1 = Integer.parseInt("" + a + b);
+        int num2 = Integer.parseInt("" + b + a);
+        if(num1 > num2) answer = num1;
+        else if (num1 < num2) answer = num2;
+        else answer = num1;
+        return answer;
+    }
+}

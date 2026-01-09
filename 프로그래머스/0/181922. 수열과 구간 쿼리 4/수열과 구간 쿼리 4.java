bb43e1fd0a47f84cda5afd94a57d79c42ce4fde8
skim257 <85626518+skim257@users.noreply.github.com>
2026-01-09 16:38:05 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 7.92 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,13 +1,27 @@
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        //int[] answer = new int[arr.length];
-        for(int i=0; i<queries.length; i++){
-            for(int j=queries[i][0]; j<=queries[i][1]; j++){
-                if(j%queries[i][2] == 0){
-                    arr[j]++;
+        int[] answer = Arrays.copyOfRange(arr, 0, arr.length);
+        int start = 0; 
+        int end = 0;
+        int multiple = 0;
+        
+        for(int j = 0; j<queries.length; j++){
+            start = queries[j][0];
+            end = queries[j][1];
+            multiple = queries[j][2];
+            
+            for(int i = 0; i<arr.length; i++){
+                if(start <= i && i <= end){
+                    if(i % multiple == 0){
+                        arr[i] = arr[i] + 1;
+                    }
                 }
             }
         }
-        return arr;
+        
+        answer = Arrays.copyOfRange(arr, 0, arr.length);
+        
+        return answer;
     }
 }
